0688836ab282cedec7b3cf2f02036589a63e390a
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2026-05-20 18:01:33 +0200

    fix: correct button click handler syntax in Alert component

--- portfolio-react/src/components/alert.tsx
+++ portfolio-react/src/components/alert.tsx
@@ -5,7 +5,7 @@ export const Alert = (props : AlertProps) => {
   return (
     <div className={`alert ${props.success ? 'alert-success' : props.info ? 'alert-info' : 'alert-error'} relative`} role="alert">
       <span className="inline alert-message px-4 py-3">{props.message}</span>
-      <button onClick={(e) => {}} 
+      <button onClick={() => {}} 
         className="text-2xl close-button leading-none top-0 left-0 h-10 w-10">&times;</button>
     </div>
   )
