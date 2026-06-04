a210990ed09507076dc13a9d53bb2f4a7142274c
Gibet <laguerrejb13@gmail.com>
2026-06-04 12:32:43 +0200

    fix: update alert component to include textured main class

--- portfolio-react/src/components/alert.tsx
+++ portfolio-react/src/components/alert.tsx
@@ -3,7 +3,7 @@ import type { AlertProps } from '../utils/types'
 export const Alert = (props : AlertProps) => {
 
   return (
-    <div className={`alert ${props.success ? 'alert-success' : props.info ? 'alert-info' : 'alert-error'} relative`} role="alert">
+    <div className={`alert textured-main ${props.success ? 'alert-success' : props.info ? 'alert-info' : 'alert-error'} relative`} role="alert">
       <span className="inline alert-message px-4 py-3 text-xs terminal">{props.message}</span>
       <button onClick={props.close} 
         className="text-2xl close-button leading-none top-0 left-0 h-10 w-10">&times;</button>
