5f2ee8508a8da745fea809066aa478bea7eebc43
Gibet <laguerrejb13@gmail.com>
2026-06-04 15:28:59 +0200

    fix: update footer layout and adjust padding for better spacing

--- portfolio-react/src/components/footer.tsx
+++ portfolio-react/src/components/footer.tsx
@@ -5,21 +5,21 @@ import { memo } from 'react'
 
 const Footer: React.FC<HeaderProps> = (props: HeaderProps) => {
   return (
-    <footer className='backdrop-blur-xs w-full 2xl:max-w-[75vw] flex gap-8 justify-center text-center pb-5 pt-6'>
-      <div className='flex flex-col w-1/3 items-start gap-2'>
+    <footer className='backdrop-blur-xs w-full 2xl:max-w-[75vw] grid grid-cols-2 justify-center items-center text-center pt-6'>
+      <div className='col-span-1 flex flex-col justify-self-center w-full px-4 items-start gap-2'>
         <CommandLine variant='tertiary' title="Menu" />
         <hr className='w-full'/>
-        <ul className='text-left text-xs'>
-          <li><button onClick={() => props.navigate('home')}><Home size={14} strokeWidth={ 1} /><span>Accueil</span></button></li>
-          <li><button onClick={() => props.navigate('about')}><Info size={14} strokeWidth={ 1} /><span>À propos</span></button></li>
-          <li><button onClick={() => props.navigate('projects')}><Cog size={14} strokeWidth={ 1} /><span>Projets</span></button></li>
-          <li><button onClick={() => props.navigate('contact')}><Contact size={14} strokeWidth={ 1} /><span>Contact</span></button></li>
+        <ul className='text-left w-full text-xs grid grid-cols-2 md:grid-cols-3 gap-3 text-nowrap'>
+          <li className='col-span-1'><button onClick={() => props.navigate('home')}><Home size={14} strokeWidth={ 1} /><span>Accueil</span></button></li>
+          <li className='md:col-span-2 col-span-1'><button onClick={() => props.navigate('about')}><Info size={14} strokeWidth={ 1} /><span>À propos</span></button></li>
+          <li className='col-span-1'><button onClick={() => props.navigate('projects')}><Cog size={14} strokeWidth={ 1} /><span>Projets</span></button></li>
+          <li className='col-span-1'><button onClick={() => props.navigate('contact')}><Contact size={14} strokeWidth={ 1} /><span>Contact</span></button></li>
         </ul>
       </div>
-      <div className='flex flex-col w-1/3 items-start gap-2'>
+      <div className='col-span-1 flex flex-col justify-self-center w-full px-4 items-start gap-2'>
         <CommandLine variant='tertiary' title="Liens" />
         <hr className='w-full'/>
-        <ul className='text-left text-xs'>
+        <ul className='text-left text-xs grid grid-cols-1 gap-3'>
           <li><a href="" target="_blank" rel="noopener noreferrer"><Github size={14} strokeWidth={ 1} /><span>GitHub</span></a></li>
           <li><a href="" target="_blank" rel="noopener noreferrer"><Linkedin size={14} strokeWidth={ 1} /><span>LinkedIn</span></a></li>
         </ul>
